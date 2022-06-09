ce194210905c6f1c8dfe8f35cbd3f509fa2334a7
Ruchitha Rajaghatta <rsr262@cornell.edu>
2022-06-08 21:23:14 -0400

    reqs completed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import "./About.css";
+import Rajaghatta_Ruchitha_Headshot from '../assets/Rajaghatta_Ruchitha_Headshot.jpg';
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={Rajaghatta_Ruchitha_Headshot}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Ruchitha Rajaghatta</div>
+            <div className="brief_description">
+              Hello! I'm Ruchitha :) I'm a rising junior at Cornell University, studying Computer Science. <br></br>
+              I love:
+              <li>problem-solving</li>
+              <li>cooking</li>
+              <li>being social!</li>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
